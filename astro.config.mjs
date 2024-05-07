5527d0e4fa023f39f7a92d66a9e7d442a9d842fe
fedeparrilla10 <federicoivanpd@gmail.com>
2024-05-08 00:15:06 +0300

    [feature] all website

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://fedeparrilla10.github.io',
+  base: '/demonacon',
+  integrations: [tailwind()],
+});
